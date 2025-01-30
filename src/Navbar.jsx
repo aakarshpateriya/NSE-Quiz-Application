ac8de9cc9288f928bcceda045c0fb7a117508d9e
Aakarsh Pateriya <aakarshpateriya123@gmail.com>
2025-01-31 03:05:01 +0530

    routing is removed

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,35 +1,26 @@
 // Navbar.jsx
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './Navbar.css';
-import SixthGradeSubjectList from './SixthGradeSubjectList';
-import LoginForm from './LoginForm';
 
 const Navbar = () => {
     return (
-        <BrowserRouter>
-            <nav className="navbar">
-                <h1 className="navbar-logo">NFC Education</h1>
-                <ul className="navbar-links">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/services">Services</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route path="/" element={<LoginForm/>} />
-                {/* Add other routes here */}
-            </Routes>
-        </BrowserRouter>
+        <nav className="navbar">
+            <h1 className="navbar-logo">NFC Education</h1>
+            <ul className="navbar-links">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/services">Services</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
     );
 };
 
